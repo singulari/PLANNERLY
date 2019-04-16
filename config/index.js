f42665c953fcbffc00219c00492387357315f4bc
Jiahui Cao <e0202898@u.nus.edu>
2019-04-16 15:30:46 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -12,27 +12,23 @@ module.exports = {
         assetsPublicPath: '/',
         proxyTable: {
             '/api': {
-                target: 'http://111.230.36.242:9997', // API服务所在IP及端口号
-                changeOrigin: true, // 如果设置为true,那么本地会虚拟一个服务器接收你的请求并代你发送该请求，这样就不会有跨域问题（只适合开发环境）
+                target: 'http://111.230.36.242:9997', 
+                changeOrigin: true, 
                 pathRewrite: {
-                    '^/api': '' // 重写路径
+                    '^/api': ''
                 }
             }
         },
 
-        //http://111.230.36.242:9997/student/token_name/cap?token_name=19735_674
-
         // Various Dev Server settings
-        host: 'localhost', // can be overwritten by process.env.HOST
-        port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+        host: 'localhost', 
+        port: 8080, 
         autoOpenBrowser: false,
         errorOverlay: true,
         notifyOnErrors: true,
-        poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
+        poll: false, 
 
-        // Use Eslint Loader?
-        // If true, your code will be linted during bundling and
-        // linting errors and warnings will be shown in the console.
+       
         useEslint: true,
         // If true, eslint errors and warnings will also be shown in the error overlay
         // in the browser.
@@ -42,12 +38,8 @@ module.exports = {
          * Source Maps
          */
 
-        // https://webpack.js.org/configuration/devtool/#development
         devtool: 'cheap-module-eval-source-map',
 
-        // If you have problems debugging vue-files in devtools,
-        // set this to false - it *may* help
-        // https://vue-loader.vuejs.org/en/options.html#cachebusting
         cacheBusting: true,
 
         cssSourceMap: true
@@ -62,23 +54,15 @@ module.exports = {
         assetsSubDirectory: 'static',
         assetsPublicPath: '/',
 
-        /**
-         * Source Maps
-         */
-
         productionSourceMap: true,
-        // https://webpack.js.org/configuration/devtool/#production
         devtool: '#source-map',
-
-        // Gzip off by default as many popular static hosts such as
-        // Surge or Netlify already gzip all static assets for you.
+        
         // Before setting to `true`, make sure to:
         // npm install --save-dev compression-webpack-plugin
         productionGzip: false,
         productionGzipExtensions: ['js', 'css'],
 
-        // Run the build command with an extra argument to
-        // View the bundle analyzer report after build finishes:
+        // Run the build command with report
         // `npm run build --report`
         // Set to `true` or `false` to always turn it on or off
         bundleAnalyzerReport: process.env.npm_config_report
